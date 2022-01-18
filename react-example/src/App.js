173786f8208828307fc45db5214a5edecbe77c9b
Hakeem <hakeem.trotmanp1@gmail.com>
2022-01-18 12:23:15 -0500

    Moved map to App.js and placed all recipes in it

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -3,38 +3,60 @@
 */
 
 // to use other components / libraries, we need to import them
-import Ingredients from './components/ingrediants.js';
+import Recipe from './components/Recipe.js';
 
 // to use a .css file, we can also import it
 import './App.css';
 
 function App() {
-
-  const title_1 = "Ramen";
-  const prep_time_1 = "20min";
-  const ingredients_1 = [
-      'Some ramen noodles',
+  //JS object is a collection of related varibles
+  const recipes = [
+    {
+        title: 'Ramen',
+        prep_time: '20 min',
+        ingredients: [
+      'some ramen noodles',
       '1 cup of water',
-      'Seasoning',
+      'seasoning',
       '2 eggs'
-    ];
-  
-    
-    const title_2 = "Curry Mutton";
-    const prep_time_2 = "40min";
-    const ingredients_2 = [
-      'Scallions',
-      'Curry powder',
-      'Salt',
-      'Hot pepper'
-    ];
+        ]
+    },
+    {
+        title: 'Curry Mutton',
+        prep_time: '40 min',
+        ingredients:  [
+      'scallions',
+      'curry powder',
+      'salt',
+      'hot pepper'
+        ]
+    },
+    {
+        title: 'Hamburger',
+        prep_time: '15 min',
+        ingredients: [
+      'burger patty',
+      'bun',
+      'ketchup',
+      'mayo',
+        ]
+    }
+      ];
 
-  return (
-    <div className='recipe-parent'>
-        <Ingredients ingredients={ingredients_1} title={title_1} prep_time={prep_time_1}/>
-        <Ingredients ingredients={ingredients_2} title={title_2} prep_time={prep_time_2}/>
-    </div>
-      );
+      return (
+        <div className='recipe-parent'>
+            {/* to access a part of a JS object, use the dot operator.
+            for example: object_name.subpart_name */}
+            {/* to pass JS variables to components, include them as an attribute.
+          for example: attribute_name={variable_name}
+              */}
+            {
+          recipes.map((recipe) => {
+              return <Recipe ingredients={recipe.ingredients} title={recipe.title} prep_time={recipe.prep_time} />;
+          })
+            }
+        </div>
+          );
   }
 
 export default App;
---
+++ react-example/src/components/Recipe.js
@@ -0,0 +1,33 @@
+import ingredients from "../App";
+
+function Recipe({
+    ingredients,
+    title,
+    prep_time
+}){
+    return (
+        <div>
+            <h2>
+                {title}
+            </h2>
+            
+            <p>
+                {prep_time}
+            </p>
+
+            <ol>    
+                {/* {ingredients} */}
+                {ingredients.map(
+                    (ingredient)=>{
+                        return <li key={ingredient}>{ingredient}</li>;
+                    
+                    }
+                )}
+            </ol>
+        </div>
+
+    );
+}
+
+// this lets other components / JS files use this file
+export default Recipe;
